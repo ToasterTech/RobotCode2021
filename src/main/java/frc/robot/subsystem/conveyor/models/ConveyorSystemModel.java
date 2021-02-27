d2794d885a740a6bd44ad9d931026443dd802402
jerestet <j.eremystettner@icloud.com>
2021-02-27 17:25:15 -0500

    created states for the conveyor

--- src/main/java/frc/robot/subsystem/conveyor/models/ConveyorSystemModel.java
+++ src/main/java/frc/robot/subsystem/conveyor/models/ConveyorSystemModel.java
@@ -6,6 +6,19 @@ package frc.robot.subsystem.conveyor.models;
  */
 public class ConveyorSystemModel extends ConveyorModel {
 
+  public enum ConveyorState {
+    INTAKE_SLOW, INTAKE_FAST, STOPPED, OUTAKE
+  }
+
+public final ConveyorState conveyorState;
+
+  public ConveyorSystemModel(ConveyorState state) {
+    this.conveyorState = state;
+  }
+
+  public ConveyorSystemModel() {
+    this.conveyorState = ConveyorState.STOPPED;
+  }
   @Override
   public boolean equals(Object other) {
     // TODO Auto-generated method stub
