b6fbc0574c25b021909231bdd3407d80a4c21afe
jerestet37 <77815228+jerestet37@users.noreply.github.com>
2021-10-23 14:29:47 -0400

    Fixed Autonomous (#13)
    * Fixed Autonomous
    
    * Adjusted motor detection speeds to fix shooting bug

--- src/main/java/frc/robot/controllers/AutoModeController.java
+++ src/main/java/frc/robot/controllers/AutoModeController.java
@@ -8,7 +8,7 @@
 package frc.robot.controllers;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import frc.robot.controllers.ConveyorStateMachine.ConveyorStateMachineInput;
 import frc.robot.models.RobotModel;
 import frc.robot.subsystem.drive.models.DifferentialDriveModel;
 import frc.robot.subsystem.shooter.models.ShooterSubsystemModel;
@@ -30,6 +30,7 @@ public class AutoModeController extends RobotStateController {
   private AutomodeState autoState;
   private Optional<Double> targetEndTime;
   private EncoderSpeedCheck encoderSpeedCheckAuto;
+  private ConveyorStateMachine conveyorStateMachine;
 
   private static final double WAIT_TIME = 0.0;
   private static final double SHOOT_TIME = 5.0 * 1000;
@@ -40,24 +41,31 @@ public class AutoModeController extends RobotStateController {
     WAIT_TO_SHOOT, SHOOT, DRIVE_BACK, STOP
   }
 
-  public AutoModeController(EncoderSpeedCheck encoderSpeedCheckAuto) {
+  public AutoModeController(EncoderSpeedCheck encoderSpeedCheckAuto, ConveyorStateMachine conveyorStateMachine) {
     this.tankDrive = new TankDrive("driverLeftAxisY", "driverRightAxisY");
     this.autoState = AutomodeState.WAIT_TO_SHOOT;
     this.targetEndTime = Optional.empty();
     this.encoderSpeedCheckAuto = encoderSpeedCheckAuto;
+    this.conveyorStateMachine = conveyorStateMachine;
   }
 
-  private RobotModel waitToShoot(double currentTime) {
+  private RobotModel waitToShoot(double currentTime, HashMap<String, InputContainer<?>> inputMap) {
     if (!targetEndTime.isPresent()) {
       targetEndTime = Optional.of(currentTime + WAIT_TIME);
     }
-    if (currentTime > this.targetEndTime.get()) {
+    if (this.encoderSpeedCheckAuto.isEncoderAtSpeed((double) inputMap.get("shooterEncoderVelocity").getValue())) {
       this.autoState = AutomodeState.SHOOT;
       targetEndTime = Optional.empty();
     }
-    return new RobotModel.RobotModelBuilder()
-                          .buildShooterModel(new ShooterSubsystemModel(ShooterState.SHOOT_DEFAULT))
-                          .build();
+    return new RobotModel.RobotModelBuilder().buildShooterModel(new ShooterSubsystemModel(ShooterState.SHOOT_DEFAULT))
+    .buildConveyorModel(this.conveyorStateMachine
+        .run(new ConveyorStateMachineInput((double) inputMap.get("conveyorSonarFront").getValue() > 1.5,
+              (double) inputMap.get("conveyorSonarMiddle").getValue() > 1.5,
+              (double) inputMap.get("conveyorSonarTop").getValue() > 1.5,
+              (double) inputMap.get("conveyorSonarIntakeCheck").getValue() > 1.5,
+              this.encoderSpeedCheckAuto.isEncoderAtSpeed((double) inputMap.get("shooterEncoderVelocity").getValue()),
+              false)))
+    .build();
   }
 
   private RobotModel shoot(double currentTime, HashMap<String, InputContainer<?>> inputMap) {
@@ -67,9 +75,18 @@ public class AutoModeController extends RobotStateController {
     SmartDashboard.putNumber("targetTime", (double)targetEndTime.get());
     if (currentTime > this.targetEndTime.get()) {
       this.autoState = AutomodeState.DRIVE_BACK;
+      // this.autoState = AutomodeState.STOP;
       targetEndTime = Optional.empty();
     }
-    return new AutomaticShoot(this.encoderSpeedCheckAuto).run(inputMap);
+    return new RobotModel.RobotModelBuilder().buildShooterModel(new ShooterSubsystemModel(ShooterState.SHOOT_DEFAULT))
+      .buildConveyorModel(this.conveyorStateMachine
+          .run(new ConveyorStateMachineInput((double) inputMap.get("conveyorSonarFront").getValue() > 1.5,
+                (double) inputMap.get("conveyorSonarMiddle").getValue() > 1.5,
+                (double) inputMap.get("conveyorSonarTop").getValue() > 1.5,
+                (double) inputMap.get("conveyorSonarIntakeCheck").getValue() > 1.5,
+                this.encoderSpeedCheckAuto.isEncoderAtSpeed((double) inputMap.get("shooterEncoderVelocity").getValue()),
+                true)))
+      .build();
   }
 
   private RobotModel driveBack(double currentTime) {
@@ -85,11 +102,22 @@ public class AutoModeController extends RobotStateController {
                           .build();
   }
 
-
   @Override
+
   public RobotModel run(HashMap<String, InputContainer<?>> inputMap) {
+    SmartDashboard.putBoolean("ConveyorFrontTirggered", (double) inputMap.get("conveyorSonarFront").getValue() > 1.5);
+    SmartDashboard.putBoolean("ConveyorMiddleTirggered", (double) inputMap.get("conveyorSonarMiddle").getValue() > 1.5);
+    SmartDashboard.putBoolean("ConveyorTopTirggered", (double) inputMap.get("conveyorSonarTop").getValue() > 1.5);
+    SmartDashboard.putBoolean("ShooterAtSpeed",
+        this.encoderSpeedCheckAuto.isEncoderAtSpeed((double) inputMap.get("shooterEncoderVelocity").getValue()));
+    SmartDashboard.putBoolean("ShooterTriggered", (boolean) inputMap.get("driverRightShoulder").getValue());
+    SmartDashboard.putNumber("BallCount", conveyorStateMachine.getBallCount());
+
+    SmartDashboard.putNumber("ConveyorFrontValue", (double) inputMap.get("conveyorSonarFront").getValue());
+    SmartDashboard.putNumber("ConveyorMiddleValue", (double) inputMap.get("conveyorSonarMiddle").getValue());
+    SmartDashboard.putNumber("ConveyorTopValue", (double) inputMap.get("conveyorSonarTop").getValue());
     switch (this.autoState) {
-      case WAIT_TO_SHOOT: return this.waitToShoot((double) inputMap.get("currentTime").getValue());
+      case WAIT_TO_SHOOT: return this.waitToShoot((double) inputMap.get("currentTime").getValue(), inputMap);
       case SHOOT: return this.shoot((double) inputMap.get("currentTime").getValue(), inputMap);
       case DRIVE_BACK: return this.driveBack((double) inputMap.get("currentTime").getValue());
       case STOP: return new RobotModel.RobotModelBuilder().buildDriveModel(new DifferentialDriveModel(0,0)).build();
